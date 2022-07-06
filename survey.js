840dcfc3f57758ca8789edc73e45fe2e69e75018
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-05 23:08:54 -0700

    console.log for a fully formed paragraph for their online profile

--- survey.js
+++ survey.js
@@ -7,26 +7,30 @@ const rl = readline.createInterface({
 
 let answers = [];
 
-rl.question ("What's your name? Nicknames are also acceptable :)", (answer) => {
+rl.question ("What's your name? Nicknames are also acceptable :) ", (answer) => {
   answers.push(answer);
 
-  rl.question ("What's an activity you like doing?", (answer) => {
+  rl.question ("What's an activity you like doing? ", (answer) => {
     answers.push(answer);
 
-    rl.question ("What do you listen to while doing that?", (answer) => {
+    rl.question ("What do you listen to while doing that? ", (answer) => {
       answers.push(answer);
 
-      rl.question ("Which meal is your favourite (eg: dinner, brunch, etc.)", (answer) => {
+      rl.question ("Which meal is your favourite (eg: dinner, brunch, etc.) ", (answer) => {
         answers.push(answer);
 
-        rl.question ("What's your favourite thing to eat for that meal?", (answer) => {
+        rl.question ("What's your favourite thing to eat for that meal? ", (answer) => {
           answers.push(answer);
 
-          rl.question ("Which sport is your absolute favourite?", (answer) => {
+          rl.question ("Which sport is your absolute favourite? ", (answer) => {
             answers.push(answer);
 
-            rl.question ("What is your superpower? In a few words, tell us what you are amazing at!", (answer) => {
+            rl.question ("What is your superpower? In a few words, tell us what you are amazing at! ", (answer) => {
               answers.push(answer);
+
+              rl.close();
+
+              console.log(`${answers[0]} loves listening to ${answers[2]} while ${answers[1]}, devouring ${answers[4]} for ${answers[3]}, prefers ${answers[5]} over any other sport, and is amazing at ${answers[6]}.`)
             })
           })
         })
@@ -34,18 +38,3 @@ rl.question ("What's your name? Nicknames are also acceptable :)", (answer) => {
     })
   })
 })
-
-
-
-// rl.question('What do you think of Node.js? ', (answer) => {
-//   console.log(`Thank you for your valuable feedback: ${answer}`);
-
-//   rl.close();
-// });
-
-
-
-// Devani loves listening to Ludovico Einaudi while coding, devouring Yak Momos for brunch, prefers Tennis over any other sport, and is amazing at dropping mad puns at inopportune times.
-
-
-
